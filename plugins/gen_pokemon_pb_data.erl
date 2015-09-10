e5da65591fe7c7616ee279ee81e91258bf202ae6
csyangchen <csyangchen@hotmail.com>
2015-09-10 11:12:21 +0800

    fix eunit errors

--- plugins/gen_pokemon_pb_data.erl
+++ plugins/gen_pokemon_pb_data.erl
@@ -5,23 +5,22 @@
 pre_compile(_, _) ->
 
     PokemonPb = "src/pokemon_pb.erl",
-
-    {ok, pokemon_pb, Bin} = compile:file(PokemonPb, [debug_info, binary]),
-    {ok, {_, [{abstract_code, {_, Forms}}]}} = beam_lib:chunks(Bin, [abstract_code]),
-
-    ModeName = "pokemon_pb_data",
-
-    Content = io_lib:format("
+    case filelib:is_file(PokemonPb) of
+        false -> ok;
+        true ->
+            {ok, pokemon_pb, Bin} = compile:file(PokemonPb, [debug_info, binary]),
+            {ok, {_, [{abstract_code, {_, Forms}}]}} = beam_lib:chunks(Bin, [abstract_code]),
+            ModeName = "pokemon_pb_data",
+            Content = io_lib:format("
 -module(~s).\n
 -export([forms/0]).\n
 forms() ->\n
 ~p.
 ", [ModeName, Forms]),
-
-    Filename = "src/" ++ ModeName ++ ".erl",
-
-    write_file(Filename, Content),
-    ok.
+            Filename = "src/" ++ ModeName ++ ".erl",
+            write_file(Filename, Content),
+            ok
+    end.
 
 write_file(FileName, Content) ->
     BinContent = iolist_to_binary(Content),
